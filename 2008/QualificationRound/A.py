578624902d5669a2e1b2b01550da92e2cf39ad2c
cynful <cynthia.m.kwok@gmail.com>
2018-03-13 22:41:59 -0700

    works for sample.in

--- 2008/QualificationRound/A.py
+++ 2008/QualificationRound/A.py
@@ -1,20 +1,27 @@
 #!/usr/bin/python
 #
-# Google-Code-Jam/2008/QualificationRound/problemA.py
+# Google-Code-Jam/2008/QualificationRound/A.py
+# Usage: python A.py < input.in > output.out
 
 def switchengines():
     numeng = input()
     listeng = [raw_input() for j in range(numeng)]
     numquer = input()
     listquer = [raw_input() for j in range(numquer)]
-    
+   
     freq =  {j:listquer.count(j) for j in listeng}
-    print freq
+
+    switches = 0
+    curreng = min(freq, key=freq.get)
+    for j in listquer:
+        freq[j] -= 1
+        if j == curreng:
+            switches += 1
+            curreng = min(freq, key=freq.get)
+
+    return int(switches)
 
 
-
-
-    return int(1)
 
 for i in range(input()):
     print "Case #%d: %d" % (i+1, switchengines())
