c4453630d1e4910d762917c10552e0ae5a4fa217
cynful <cynthia.m.kwok@gmail.com>
2018-03-23 23:58:44 -0700

    round 1A problemA solved

---
+++ 2008/Round1A/ProblemA/A.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+#
+# google-code-jam/2008/QualificationRound/ProblemA/A.py
+# Usage: python A.py < input.in > output.out
+
+def minscalarproduct():
+    variables = input()
+    x = map(int, raw_input().split())
+    y = map(int, raw_input().split())
+   
+    x.sort()
+    y.sort()
+    y.reverse()
+
+    product = 0
+    for j in range(variables):
+        product += x[j]*y[j]
+    return product
+
+
+
+for i in range(input()):
+    print "Case #%d: %d" % (i+1, minscalarproduct())
